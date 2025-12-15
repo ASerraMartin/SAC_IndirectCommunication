16c3c4ebdf3f6945b80172c950e73cde6afe8fa6
Adrià <aserram2004@hotmail.com>
2025-12-15 01:49:52 +0100

    monitor to ckeck all message interchanges

---
+++ monitor.py
@@ -0,0 +1,73 @@
+import zmq
+import sys
+import interface
+import threading
+from config import X_PORT, O_PORT
+
+move_to_ack = None
+
+
+def start_monitor():
+    context = zmq.Context()
+    listener = context.socket(zmq.SUB)
+
+    # Subscribe to all topics
+    listener.connect(f"tcp://localhost:{X_PORT}")
+    listener.connect(f"tcp://localhost:{O_PORT}")
+    listener.subscribe("")
+    print("Monitor started. Waiting for moves...")
+
+    while True:
+        try:
+            msg = listener.recv_multipart()
+            topic, payload = msg
+            topic = topic.decode()
+            payload = payload.decode()
+
+            # Move handling
+            if topic in ["X", "O"]:
+                print(f"[{topic}] Move: {payload}")
+                move_to_ack = topic
+
+            # Move confirmation handling
+            elif topic == "ok":
+                print(f"[{topic}] Move acknowledged: {payload}")
+                row_str, col_str = payload.split(",")
+                interface.mark_square(int(row_str), int(col_str), move_to_ack)
+                move_to_ack = None
+
+            # Error handling
+            elif topic == "error":
+                print(f"[{topic}] Error reported: {payload}")
+
+            # Game end handling
+            elif topic == "end":
+                if payload == "Draw":
+                    print(f"[{topic}] GAME OVER | Draw")
+                else:
+                    print(f"[{topic}] GAME OVER | Winner: {payload}")
+
+        # Handle ZeroMQ errors
+        except zmq.ZMQError as e:
+            print(f"Publisher-subscriber error: {e}")
+            break
+
+        # Handle other exceptions
+        except Exception as e:
+            print(f"Error in the monitor loop: {e}")
+            break
+
+
+if __name__ == "__main__":
+
+    # Monitor runs the GUI and a background thread for the publisher-subscriber
+    threading.Thread(target=start_monitor, daemon=True).start()
+
+    try:
+        interface.run()
+
+    # Handle ctrl+C or manual interruption to close the game
+    except KeyboardInterrupt:
+        print("Monitor closing...")
+        interface.window.destroy()
+        sys.exit(0)
